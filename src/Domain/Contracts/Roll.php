baaf8688817efda0c49666121a9e81eeefa55dce
Maikel Enrique Pernía Matos <mpernia@farmadosis.com>
2024-03-09 18:52:52 +0100

    fix Roll interface

--- src/Domain/Contracts/Roll.php
+++ src/Domain/Contracts/Roll.php
@@ -5,6 +5,16 @@ namespace PouchScanner\Domain\Contracts;
 interface Roll
 {
     public function getPatientRoll(): ?string;
+
     public function getBatchId(): ?string;
+    
     public function getPatientId(): ?string;
+
+    public function getPouches(): ?PouchCollection;
+
+    public function setPouches(?PouchCollection $pouches): Roll:
+    
+    public function getStatus(): string;
+    
+    public function setStatus(string $status): Roll
 }
